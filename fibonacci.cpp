f429fb1ad0a243f2cce7d70df53df21f31839203
Zhanaidar Mukanov <zhanaidar@Zhanaidars-MacBook-Pro.local>
2021-07-19 17:14:30 +0800

    Bone of program

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+//
+//  Created by Zhanaidar Mukanov
+//  Copyright © 2021 Zhanaidar Mukanov. All rights reserved.
+//
+
+// All Required Libraries
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int K = 0;K < T;K++)
+    {
+        
+    }
+    system("pause");
+    return 0;
+}
