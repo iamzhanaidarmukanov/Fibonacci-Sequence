b9dfc010fbf05fda8a894bf55a21bb95deec5f8c
Zhanaidar Mukanov <zhanaidar@Zhanaidars-MacBook-Pro.local>
2021-07-23 22:53:41 +0800

    Implemeted Counting sort with rightshift

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -7,39 +7,76 @@
 #include <iostream>
 using namespace std;
 
-int64_t mod(const int64_t& x, const int64_t& y, const int64_t& Q) {
-    int64_t z = (x + y) % Q;
+
+long long mod(const long long& x, const long long& y, const long long& Q) {
+    long long z = (x + y) % Q;
     return z;
 }
 
-int64_t getMax(int32_t sequence[], int64_t N, int64_t Q) {
-    int64_t max = 1;
-    sequence[0] = 1;
-    sequence[1] = 1;
-    
-    for (int64_t i = 2; i < N; ++i) {
-        sequence[i] = mod(sequence[i - 1], sequence[i - 2], Q);
-        if (max < sequence[i])
-            max = sequence[i];
+
+long long getMax(long firstSequence[], const long& N, const long& Q) {
+    long long max = 1;
+    firstSequence[0] = 1;
+    firstSequence[1] = 1;
+    for (int i = 2;i < N; i++) {
+        firstSequence[i] = mod(firstSequence[i - 1], firstSequence[i - 2], Q);
+        if (max < firstSequence[i])
+            max = firstSequence[i];
     }
     return max;
 }
 
-int main() {
-    
-    int number_of_testcases;
-    cin >> number_of_testcases;
-    
-    for (int testcase = 0; testcase < T; ++testcase) {
-        int64_t *sequence_one, *sequence_two;
-        sequence_one = new int64_t[500000];
-        sequence_two = new int64_t[500000];
-        int64_t N, Q;
-        int64_t max;
-        cin >> N >> Q;
-        max = getMax(sequence_one, N, Q);
-        
+
+void countingSort(long firstSequence[], long secondSequence[], const long& N, const long& rightShift) {
+
+    long  *pointr = new long[1024];
+
+    for (int i = 0; i < 1024; i++) {
+        pointr[i] = 0;
     }
-    system("pause");
-    return 0;
+    
+    for (int i = 0; i < N; i++) {
+        pointr[(firstSequence[i] >> rightShift) % 1024]++;
+        secondSequence[i] = firstSequence[i];
+    }
+    for (int i = 1;i < 1024; i++) {
+        pointr[i] += pointr[i - 1];
+    }
+    for (long i = N - 1;i >= 0;i--) {
+        firstSequence[pointr[(secondSequence[i] >> rightShift) % 1024] - 1] = secondSequence[i];
+        --pointr[(secondSequence[i] >> rightShift) % 1024];
+    }
+}
+
+int main() {
+    int T;
+    cin >> T;
+    for (int K = 0;K < T;K++)
+    {
+        long *firstSequence, *secondSequence;
+
+        firstSequence = new long[5000000];
+        secondSequence = new long[5000000];
+
+        long N, Q;
+        long max;
+
+        cin >> N  >> Q;
+        max = getMax(firstSequence, N,  Q);
+
+        for (long long i = 0; (max >> i) > 0; i += 10)
+            countingSort(firstSequence, secondSequence, N,  i);
+
+        long long sum = 0;
+
+        for (long long i = 0; i < N; i++)
+        {
+            sum += (i + 1)* firstSequence[i];
+            sum %= Q;
+        }
+
+        cout << sum << endl;
+    }
+
+    return -1;
 }
